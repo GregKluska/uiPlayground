dd0b5abfcdb6d4d838f9c4671ef7011a6068e31e
Greg Kluska <kluskagreg@gmail.com>
2020-12-09 21:38:06 +0000

    Initial commit

---
+++ app/src/main/java/com/gregkluska/playground/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gregkluska.playground.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gregkluska.playground.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
