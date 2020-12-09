80a8c9ac1b88ca1b946ddd0e1de9e5f870b03ac0
Greg Kluska <kluskagreg@gmail.com>
2020-12-09 23:18:22 +0000

    Add Hilt

---
+++ app/src/main/java/com/gregkluska/playground/presentation/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.gregkluska.playground.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application(){
+
+}
---
+++ app/src/main/java/com/gregkluska/playground/presentation/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.gregkluska.playground.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            )
+            {
+                Text(text = "Hey look some text")
+                Spacer(modifier = Modifier.padding(top=10.dp))
+                Button(
+                    onClick = {}
+                ) {
+                    Text(text = "A BUTTON")
+                }
+            }
+        }
+    }
+}
